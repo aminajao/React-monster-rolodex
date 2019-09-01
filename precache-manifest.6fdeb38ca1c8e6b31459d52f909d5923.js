39e2faa0fdb013218b246ff02cf28add2fb09290
Amin Ajao <aminajao96@gmail.com>
2019-09-01 17:43:26 +0100

    Updates

---
+++ precache-manifest.6fdeb38ca1c8e6b31459d52f909d5923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9b4d0ce7332aaf9e0e7990fa48bf11",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "ff88d4d45305fa36c5b9",
+    "url": "/react-monster-rolodex/static/css/main.f1a481e4.chunk.css"
+  },
+  {
+    "revision": "f802219fca84ad4640ab",
+    "url": "/react-monster-rolodex/static/js/2.ea1710b8.chunk.js"
+  },
+  {
+    "revision": "ff88d4d45305fa36c5b9",
+    "url": "/react-monster-rolodex/static/js/main.23c2af95.chunk.js"
+  },
+  {
+    "revision": "4f78b77805659e649268",
+    "url": "/react-monster-rolodex/static/js/runtime~main.4b42415a.js"
+  }
+]);
